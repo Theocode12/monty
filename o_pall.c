fff031302a5b0d50aa33796757eb17a990e1e9a3
Theocode12 <vchidubem9@gmail.com>
2022-06-15 08:12:24 +0100

    task 1 test

--- o_pall.c
+++ o_pall.c
@@ -3,15 +3,9 @@
 void o_pall(stack_t **top, __attribute__((unused))unsigned int line_number)
 {
 	stack_t *temp = *top;
-	char *args = strtok(NULL, " \t\n\r");
 	
 	if (temp == NULL)
 		return;
-	if (args != NULL)
-	{
-		fprintf(stderr,"L%d: unknown instruction %s\n", line_number, args);
-		exit(EXIT_FAILURE);
-	}
 	while (temp)
 	{
 		printf("%d\n", temp->n);
