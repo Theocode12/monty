e34d305a79626ceb6148dfaad50b3c2aa9d43729
Theocode12 <vchidubem9@gmail.com>
2022-06-15 16:24:50 +0100

    pint bytecode

--- o_pint.c
+++ o_pint.c
@@ -10,7 +10,7 @@ void o_pint(stack_t **top, unsigned int line_number)
 {
 	if (*top == NULL)
 	{
-		fprintf(stderr, "L%d: can't pint, stack", line_number);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*top)->n);
