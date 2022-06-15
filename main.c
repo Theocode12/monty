a9806818a362039cc8edeaa2a90c56677b58bb0a
Theocode12 <vchidubem9@gmail.com>
2022-06-15 09:04:16 +0100

    task 1 test

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(int argc, char **argv)
 	FILE *stream = NULL;
 	stack_t *top = NULL;
 
-	if (argc <= 1)
+	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
